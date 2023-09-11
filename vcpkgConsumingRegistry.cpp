53b93ed6cb7cf7526410b0045bcd6e33371d9c8d
urhol1 <martin.holcik@rieter.com>
2023-09-11 14:38:41 +0200

    cleanup

--- vcpkgConsumingRegistry.cpp
+++ vcpkgConsumingRegistry.cpp
@@ -2,11 +2,14 @@
 //
 
 #include "vcpkgConsumingRegistry.h"
+//#include "out/build/linux-debug-vcpkg/vcpkg_installed/x64-linux/include/lely/util/bits.h"
+#include "bits.h"
 
 using namespace std;
 
 int main()
 {
+	clz8(10);
 	cout << "Hello CMake." << endl;
 
 	return 0;
