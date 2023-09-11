fcb7b7d933ef7cc0aa1fcad72124f347e1485079
urhol1 <martin.holcik@rieter.com>
2023-09-11 10:15:32 +0200

    repo init

---
+++ vcpkgConsumingRegistry.cpp
@@ -0,0 +1,13 @@
+﻿// vcpkgConsumingRegistry.cpp : Defines the entry point for the application.
+//
+
+#include "vcpkgConsumingRegistry.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	return 0;
+}
